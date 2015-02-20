110bba806780d7af311c3fe2f4fa63f255320aac
janiner <janinerogando@yahoo.com>
2015-02-20 18:23:16 +0800

    updated

--- project/main.py
+++ project/main.py
@@ -2,20 +2,25 @@ import pygame, sys, random, glob
 import load
 import slidemenu
 
+
+
 screen = pygame.display.set_mode((640, 400))
 clock = pygame.time.Clock()
 pygame.display.set_caption("EM'S RUSH")
-FPS = 20
+
 BLUE = (0, 0, 255)
 lovely_BLUE = (47, 164, 245)
 RED = (255, 0, 0)
 white = (255,255,255)
-    
+moveX=0    
 def scroll(x, direction):
     if direction == "left":
         x = x - 1
     elif direction == "right":
-        x = x + 4
+        x = x + 1
+
+    elif direction =="rights":
+        x=x+4
     return (x)
 
 
@@ -37,11 +42,86 @@ class Obstacle(pygame.sprite.Sprite):
         self.rect.left = 480
 
     def update(self):
+        self.rect.left -= 13
+class Obstacles(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("obstacles/poop.png")
+        self.rect = self.image.get_rect()
+        self.rect.bottom = 100
+        self.rect.left = 480
+
+    def update(self):
         self.rect.left -= 8
-        
+class Pauseb(pygame.sprite.Sprite):
+    blue = (0, 0, 255)
+    def __init__(self,color= blue, width=60, height=60):
+        super(Pauseb, self).__init__()
+        self.image = pygame.Surface((width,height))
+        self.image.fill(color)
+        self.rect=self.image.get_rect()
+
+    def set_position(self,x,y):
+        self.rect.x=x
+        self.rect.y=y
+
+    def set_image(self,filename=None):
+        if (filename!=None):
+            self.image=pygame.image.load(filename)
+            self.rect=self.image.get_rect()
+
+                
+    def pressed(self,mouse):
+        if mouse[0]==self.rect.x:
+            if mouse[1]==self.rect.y:
+                return True
+            else: return False
+        else: return False
+    
+class Sound(pygame.sprite.Sprite):
+    blue = (0, 0, 255)
+    def __init__(self,color= blue, width=60, height=60):
+        super(Sound, self).__init__()
+        self.image = pygame.Surface((width,height))
+        self.image.fill(color)
+        self.rect=self.image.get_rect()
+
+        self.sound = pygame.mixer.Sound("sounds-temporary/rush.wav")
+
+    def playsound(self):
+        self.sound.play(-1)
+
+    def set_position(self,x,y):
+        self.rect.x=x
+        self.rect.y=y
+
+    def set_image(self,filename=None):
+        if (filename!=None):
+            self.image=pygame.image.load(filename)
+            self.rect=self.image.get_rect()
+
+    def stopsound(self):
+        self.sound.stop()
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("others/coin.png")
+        self.rect = self.image.get_rect()
+        self.rect.bottom = 100
+        self.rect.left = 420
+
+    def update(self):
+        self.rect.left -= 11
+
+    #def erase(self):
+     #   self
+
+    
+
 def nextLevel():
     RED= (255, 0, 0)
-    lev_font = pygame.font.Font("basefont.ttf", 38)
+    lev_font = pygame.font.Font("font/DK Pundak.otf", 38)
     congrats = lev_font.render("Moving to", True, RED)
     level_surface = lev_font.render("Next Level", True, RED)
     surface = pygame.Surface((640, 400))
@@ -69,20 +149,27 @@ def pause():
                     quit()
         pygame.display.update()
         clock.tick(5)
+
+
 def gameover():
     RED = (255, 0, 0)
+    WHITE = (255,255,255)
     go = pygame.image.load("others/gameover.png")
-    backsurf = pygame.Surface((640, 400))
+    #backsurf = pygame.Surface((640, 400))
     #backsurf.blit(go, (0,0))            
     #screen.blit(backsurf, (0,0))
-    ouch_font = pygame.font.Font("font/Dk Pundak.otf", 76)
-    ouch_surf = ouch_font.render("OUCH!!!", True, RED)
+    #ouch_font = pygame.font.Font("font/Dk Pundak.otf", 76)
+    #ouch_surf = ouch_font.render("OUCH!!!", True, RED)
     #nextScreen = nextLevel()
     #backsurf.blit(nextScreen, (0, 0))
     #backsurf.blit(ouch_surf, (0, 0))
-    screen.blit(go, (0,0))
-    
-    
+    text=pygame.font.Font("font/Dk Pundak.otf",30)
+    text_appear=text.render("Game Over" , True, RED)
+    block = {'rect':pygame.Rect(200, 80, 230, 250),'color':WHITE}
+    pygame.draw.rect(screen, block['color'], block['rect'])
+    screen.blit(text_appear,(230,100))
+    #screen.blit(go, (190,100))
+   
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -106,6 +193,8 @@ class Player(pygame.sprite.Sprite):
 	self.fallspeed = 0
 	self.falling = False
 	self.hitBool = 0
+	self.lives = 1
+	
 
 	# If hit, the player will be knocked in the opposite direction he was last moving
     def gotHit(self):
@@ -125,7 +214,7 @@ class innerPlayer(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
 def jump_update(y, jump, number):
-    jump_speed = 15
+    jump_speed = 20
     if number == 1:
         if jump == "up" and y < 50:
             jump = "down"
@@ -141,10 +230,10 @@ def jump_update(y, jump, number):
     elif jump == "down":
         y = y + jump_speed
     return(y, jump, number)
-
+    
 def load_pavement(pavement):
     pavement.empty()
-    for x in range(0, 645, 32):
+    for x in range(0, 650, 32):
         ground = Ground()
         ground.rect.left = x
         pavement.add(ground)
@@ -154,7 +243,10 @@ def main():
     screen = pygame.display.set_mode((640, 400))
     clock = pygame.time.Clock()
     pygame.display.set_caption("EM'S RUSH")
-    FPS = 20
+    icon = pygame.image.load('logo/icon.png')
+    pygame.display.set_icon(icon)
+    FPS = 12
+    FFPS =20
     BLUE = (0, 0, 255)
     lovely_BLUE = (47, 164, 245)
     RED = (255, 0, 0)
@@ -163,7 +255,7 @@ def main():
 #load.Credits(screen)
 
     gameOver = False
-    background = pygame.image.load("background/2bg.jpg")
+    background = pygame.image.load("background/Background_final.png")
     back_rect = background.get_rect()
     max_x = back_rect.right - 600
     backsurf = pygame.Surface((640, 400))
@@ -174,7 +266,7 @@ def main():
     x = 0
 
     direction = "right"
-
+    lives = 1
     lovely = Player()
     innerLovely = innerPlayer()
 
@@ -183,6 +275,23 @@ def main():
     obstacle_group.add(poop)
     obstacle_spawn_delay = 10
 
+    poops = Obstacles()
+    obstacles_group = pygame.sprite.Group()
+    obstacles_group.add(poop)
+    obstacles_spawn_delay = 10
+
+    coin = Coin()
+    coin_group = pygame.sprite.Group()
+    coin_group.add(coin)
+    coin_spawn_delay = 5
+
+    font = pygame.font.Font("font/DK Pundak.otf", 24)
+    coins=0
+    text = font.render("Score: %s" %(coins), 1, (10,15,30))
+    screen.blit(text, (510,10))
+
+    
+
     pavement = pygame.sprite.Group()
 
     ground_counter = 32
@@ -190,6 +299,19 @@ def main():
     pavement = load_pavement(pavement)
 
 
+    coinz= pygame.image.load("others/coin.png").convert_alpha()
+
+    pauseg=pygame.sprite.Group()
+    a_pause=Pauseb()
+    a_pause.set_image("others/pause.png")
+    a_pause.set_position(10,10)
+
+    sounds=pygame.sprite.Group()
+    a_sound=Sound()
+    a_sound.set_image("others/soundOn.png")
+    a_sound.set_position(65,10)
+    a_sound.playsound()
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -197,22 +319,85 @@ def main():
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    direction = "right"
+                    direction = "rights"
+                    
                 if event.key == pygame.K_LEFT:
                     direction = "left"
                 if lovely.jump != "up" or lovely.jump != "down":
                     if event.key == pygame.K_SPACE:
+                        pygame.mixer.music.load('sounds-temporary/jump.mp3')
+                        pygame.mixer.music.play()
                         if lovely.jump_number < 2:
                             lovely.jump = "up"
                             lovely.jump_number += 1
-                if event.key == pygame.K_p:
+                #if event.key == pygame.K_p:
+                 #   pause()
+            #music = pygame.mixer.Sound("sounds-temporay/jum.mp3")
+            #pygame.mixer.music.load('sounds-temporary/rush.wav')
+            #pygame.mixer.music.play()
+            #music.play() 
+            if (event.type==pygame.MOUSEMOTION):
+                mouse= pygame.mouse.get_pos()
+                pb=pygame.image.load("others/pause.png")
+                pauser=pb.get_rect()
+                pp=pygame.transform.scale(pb,(70,70))
+                if(event.pos[0] < a_pause.rect.x+40  and event.pos[1]>a_pause.rect.y and event.pos[0]>a_pause.rect.x and event.pos[1]<a_pause.rect.y+40):
+                    screen.blit(pp,(10,10))
+                    
+            if (event.type ==pygame.MOUSEBUTTONDOWN):
+                mouse= pygame.mouse.get_pos()
+                #if (event.pos[0]== 10 ):
+                if(event.pos[0] < a_pause.rect.x+40  and event.pos[1]>a_pause.rect.y and event.pos[0]>a_pause.rect.x and event.pos[1]<a_pause.rect.y+40):
+                #if a_pause.pressed(mouse):
                     pause()
+                if(event.pos[0] < a_sound.rect.x+50  and event.pos[1]>a_sound.rect.y and event.pos[0]>a_sound.rect.x and event.pos[1]<a_sound.rect.y+50):
+                    
+                    print("off")
+                    a_sound.set_image("others/soundOff.png")
+                    a_sound.set_position(65,10)
+                    a_sound.stopsound()
+                    #if(event.pos[0] < a_sound.rect.x+50  and event.pos[1]>a_sound.rect.y and event.pos[0]>a_sound.rect.x and event.pos[1]<a_sound.rect.y+50):
+#                        a_sound.set_image("others/soundOn.png")
+ #                       a_sound.set_position(65,10)
+  #                      a_sound.playsound()
+                #if(event.pos[0] < a_sound.rect.x+50  and event.pos[1]>a_sound.rect.y and event.pos[0]>a_sound.rect.x and event.pos[1]<a_sound.rect.y+50):
+                 #   print('sound')
+                    
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    direction = "right"
+                    
+                if event.key == pygame.K_LEFT:
+                    direction = "left"
+                #if event.key == pygame.K_p:
+                 #   paused=True
+            if (event.type==pygame.MOUSEMOTION):
+                mouse= pygame.mouse.get_pos()
+                pb=pygame.image.load("others/pause.png")
+                pauser=pb.get_rect()
+                pp=pygame.transform.scale(pb,(90,90))
+
+                print(mouse)
+                if(event.pos[0] < a_pause.rect.x+40  and event.pos[1]>a_pause.rect.y and event.pos[0]>a_pause.rect.x and event.pos[1]<a_pause.rect.y+40):
+                    screen.blit(pp,pauser)
+                #if(event.pos[0] < a_sound.rect.x+50  and event.pos[1]>a_sound.rect.y and event.pos[0]>a_sound.rect.x and event.pos[1]<a_sound.rect.y+50):
+                    
+                    
+            if (event.type ==pygame.MOUSEBUTTONUP):
+                mouse = pygame.mouse.get_pos()
+                #mouse= pygame.mouse.get_pos()
+                #if (event.pos[0]== 10 ):
+                if(event.pos[0] < a_pause.rect.x+40  and event.pos[1]>a_pause.rect.y and event.pos[0]>a_pause.rect.x and event.pos[1]<a_pause.rect.y+40):
+                #if a_pause.pressed(mouse):
+                    paused=True
         if x > max_x:
             gameOver = True
         if gameOver:
-            nextScreen = nextLevel()
-            screen.blit(nextScreen, (0, 0))
+            #nextScreen = nextLevel()
+            #screen.blit(nextScreen, (0, 0))
+            print 'g'
         elif not gameOver:
+            
             x = scroll(x, direction)
             backsurf.blit(background, (0,0), (x, 0, 640 + x, 420))            
             screen.blit(backsurf, (0,0))
@@ -221,6 +406,17 @@ def main():
             backsurf.blit(background, (0,0), (x, 0, 640 + x, 420))            
             screen.blit(backsurf, (0,0))
 
+            
+    
+            
+
+            pauseg.add(a_pause)
+            pauseg.draw(screen)
+
+            
+            sounds.add(a_sound)
+            sounds.draw(screen)
+    
             lovely.rect.top, lovely.jump, lovely.jump_number = jump_update(lovely.rect.top, lovely.jump, lovely.jump_number)
             screen.blit(lovely.image, lovely.rect)
             if lovely.img_position < 2:
@@ -247,16 +443,54 @@ def main():
                 obstacle_spawn_delay -= 1
             else:
                 obstacle_group.add(Obstacle())
-                obstacle_spawn_delay = random.randrange(80, 140, 5)
+                obstacle_spawn_delay = random.randrange(40, 100, 10)
             obstacle_group.update()
             obstacle_group.draw(screen)
-
-
+            
             innerLovely.rect.center = lovely.rect.center
             if pygame.sprite.spritecollideany(innerLovely, obstacle_group):
+                lives=lives-1
+                if (lives==0):
             #print("collision")
-                gameover()
+                    gameover()
+                    #gameOver = True
+                    
             
-                clock.tick(FPS)
+            for coin in coin_group:
+                if coin.rect.right < 0:
+                    coin_group.remove(coin)
+                    
+            if coin_spawn_delay > 0:
+                coin_spawn_delay -= 1
+            else:
+                coin_group.add(Coin())
+                coin_spawn_delay = random.randrange(50, 140, 5)
+            coin_group.update()
+            coin_group.draw(screen)
+
+            innerLovely.rect.center = lovely.rect.center
+            
+            if pygame.sprite.spritecollideany(innerLovely, coin_group):
+                #if (lives==1):
+                coin_group.remove(coin)
+                coins = coins + 1
+                lives=1
+            #else:
+             #   lives=lives-0
+              #  if(lives==0):
+               #     gameover()
+                #    print('gg')
+            text = font.render("Score: %s" %(coins), 1, (10,15,30))
+            screen.blit(text, (510,10))
+
+            #if obstacles_spawn_delay > 0:
+             #   obstacles_spawn_delay -= 1
+            #else:
+             #   obstacles_group.add(Obstacles())
+              #  obstacles_spawn_delay = random.randrange(80, 140, 5)
+            #obstacles_group.update()
+            #obstacles_group.draw(screen)  
+            
+        clock.tick(FPS)
+        pygame.display.flip()
         pygame.display.update()
-    game_intro()
